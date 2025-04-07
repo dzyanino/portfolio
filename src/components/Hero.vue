311ffbd732802a68bc41117c05a06441bf51ba6b
dzyanino <dzyanino@gmail.com>
2025-04-07 18:20:11 +0300

    Add : Hero component

---
+++ src/components/Hero.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup></script>
+
+<template>
+  <v-container
+    class="h-screen py-16"
+  >
+    <div class="h-100 d-flex flex-column align-start justify-space-between">
+      <div class="d-flex flex-column align-start justify-center">
+        <span class="text-h4 font-italic">Salama ! :)</span><br>
+        <span class="text-body-1">I'm Doneli Dzyanino, a software developer</span><br>
+        <span class="text-body-2">I build things using Vue, Nuxt, Laravel, C#, Flutter... and many more</span>
+        <span class="text-body-2">You can email me at <a href="mailto:dzyanino@gmail.com">dzyanino@gmail.com</a></span>
+      </div>
+      <span class="text-caption">By the way, this is a <a
+        target="_blank"
+        href="https://en.wikipedia.org/wiki/Work_in_process"
+      >WIP</a> website. More content comming soon</span>
+    </div>
+  </v-container>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,9 @@
 <template>
-  <div>
-    <p>test</p>
+  <div class="bg-grey-lighten-2">
+    <Hero />
   </div>
 </template>
 
 <script lang="ts" setup>
-  //
+import Hero from '@/components/Hero.vue'
 </script>
