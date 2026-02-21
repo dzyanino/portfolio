fe69e0042ab266668df281e7e9b71b682d85a4ff
dzyanino <dzyanino@gmail.com>
2026-02-22 00:44:10 +0300

    Edit : changed main color to violet

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,15 @@ import ui from "@nuxt/ui/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		vue(),
-		ui({
-			ui: {
-				colors: {
-					primary: "green",
-					neutral: "zinc",
-				},
-			},
-		}),
-	],
+  plugins: [
+    vue(),
+    ui({
+      ui: {
+        colors: {
+          primary: "violet",
+          neutral: "slate",
+        },
+      },
+    }),
+  ],
 });
