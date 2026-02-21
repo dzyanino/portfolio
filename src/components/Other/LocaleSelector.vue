c50ef2816957ff9e60eb9972084e7cdefc9cd6ea
dzyanino <dzyanino@gmail.com>
2026-02-21 04:57:29 +0300

    Add : locale selector component

---
+++ src/components/Other/LocaleSelector.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import type { SelectItem } from "@nuxt/ui";
+
+const locales = ref<SelectItem[]>([
+  { label: "English", value: "en" },
+  { label: "Français", value: "fr" },
+]);
+</script>
+
+<template>
+  <USelect v-model="$i18n.locale" :items="locales" />
+</template>
