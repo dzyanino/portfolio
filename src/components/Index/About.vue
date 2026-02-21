ee8c7ae426989b65bd4aa1a6761ae7cad23d6dde
dzyanino <dzyanino@gmail.com>
2026-02-22 00:46:54 +0300

    Add : about section

---
+++ src/components/Index/About.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { useI18n } from "vue-i18n";
+import type { PageFeatureProps } from "@nuxt/ui";
+import { lgAndLarger } from "../../plugins/breakpoints";
+
+const { t, tm } = useI18n();
+</script>
+
+<template>
+  <UPageSection
+    :headline="t('about.headline')"
+    :title="t('about.title')"
+    :description="t('about.description')"
+    :orientation="lgAndLarger ? 'horizontal' : 'vertical'"
+    reverse
+    :features="tm('about.features') as PageFeatureProps[]"
+  >
+    <img
+      src="/src/assets/images/self-portrait.png"
+      width="352"
+      height="647"
+      alt="Illustration"
+      class="w-full rounded-lg"
+    />
+  </UPageSection>
+</template>
