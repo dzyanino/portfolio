856aa3496f7576dea6bd569c2d0222ae89722f96
dzyanino <dzyanino@gmail.com>
2026-02-22 00:47:17 +0300

    Add : contact section

---
+++ src/components/Index/Contact.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { useI18n } from "vue-i18n";
+import type { ButtonProps } from "@nuxt/ui";
+
+const { t, tm } = useI18n();
+</script>
+
+<template>
+  <UPageSection>
+    <UPageCTA
+      :title="t('contact.title')"
+      :description="t('contact.description')"
+      variant="soft"
+      :links="tm('contact.links') as ButtonProps[]"
+    />
+  </UPageSection>
+</template>
